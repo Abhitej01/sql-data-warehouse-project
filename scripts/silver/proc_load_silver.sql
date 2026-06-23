bb3be73fc7bc9452af5ddac35592d2a451ebe6fc
Abhi Tej Reddy <abhitej548@gmail.com>
2026-06-24 00:31:33 +0530

    Create stored procedure to load silver layer
    This stored procedure performs the ETL process to populate the 'silver' schema tables from the 'bronze' schema, including data transformation and cleansing.

---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,199 @@
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    CALL Silver.load_silver();
+===============================================================================
+*/
+
+CREATE OR REPLACE PROCEDURE silver.load_silver()
+LANGUAGE plpgsql
+AS $$
+	DECLARE
+    start_time TIMESTAMP;
+    end_time TIMESTAMP;
+    load_duration INTERVAL;
+BEGIN
+	start_time := clock_timestamp();
+	RAISE NOTICE '=====================================';
+	RAISE NOTICE 'START OF SILVER LAYER';
+	RAISE NOTICE '=====================================';
+
+	RAISE NOTICE 'Loading data into CRM Tables';
+	
+	TRUNCATE TABLE silver.crm_cust_info;
+	-- inserting data into silver.crm_cust_info
+	INSERT INTO silver.crm_cust_info(
+		cst_id,
+		cst_key,
+		cst_firstname,
+		cst_lastname,
+		cst_marital_status,
+		cst_gndr,
+		cst_create_date
+	)
+	SELECT
+		cst_id,
+		cst_key,
+		TRIM(cst_firstname) AS cst_firstname,
+		TRIM(cst_lastname) AS cst_lastname,
+	CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	 	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+		  ELSE 'n/a'
+	END cst_marital_status,
+	CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+		  ELSE 'n/a'
+	END cst_gndr,
+	cst_create_date
+	FROM (
+		SELECT
+		*,
+		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+		FROM bronze.crm_cust_info
+		WHERE cst_id IS NOT NULL
+	)t WHERE flag_last = 1;
+	
+	TRUNCATE TABLE silver.crm_prd_info;
+	-- inserting data into silver.crm_prd_info
+	INSERT INTO silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+	)
+	SELECT 
+		prd_id,
+	REPLACE(SUBSTRING(prd_Key, 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key FROM 7) AS prd_key,
+	prd_nm,
+	COALESCE(prd_cost, 0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line))
+		 WHEN 'R' THEN 'Road'
+		 WHEN 'S' THEN 'Other Sales'
+		 WHEN 'M' THEN 'Mountain'
+		 WHEN 'T' THEN 'Touring'
+		 ELSE 'n/a'
+	END AS prd_line,
+	prd_start_dt,
+	LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt
+	FROM bronze.crm_prd_info;
+	
+	TRUNCATE TABLE silver.crm_sales_details;
+	-- inserting data into silver.crm_sales_details
+	INSERT INTO silver.crm_sales_details(
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+	)
+	SELECT 
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt::TEXT) <> 8 THEN NULL
+	ELSE TO_DATE(sls_order_dt::TEXT, 'YYYYMMDD')
+	END  sls_order_dt,
+	CASE WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt::TEXT) <> 8 THEN NULL
+	ELSE TO_DATE(sls_ship_dt::TEXT, 'YYYYMMDD')
+	END  sls_ship_dt,
+	CASE WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::TEXT) <> 8 THEN NULL
+	ELSE TO_DATE(sls_due_dt::TEXT, 'YYYYMMDD') 
+	END sls_due_dt,
+	CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+	END sls_sales,
+	CASE WHEN sls_quantity IS NULL OR sls_quantity <= 0 THEN sls_sales / NULLIF(sls_price, 0)
+	ELSE sls_quantity
+	END sls_quantity,
+	CASE WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity,0)
+	ELSE sls_price
+	END sls_price
+	FROM bronze.crm_sales_details;
+
+	RAISE NOTICE 'Loading data into ERP Tables';
+	
+	TRUNCATE TABLE silver.erp_cust_az12;
+	-- inserting data into silver.erp_cust_az12
+	INSERT INTO silver.erp_cust_az12(
+	cid,
+	bdate,
+	gen
+	)
+	SELECT 
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4, LENGTH(cid))
+		 ELSE cid
+	END cid,
+	CASE WHEN bdate > CURRENT_DATE THEN NULL
+		 ELSE bdate
+	END bdate,
+	CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+		 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+		 ELSE 'n/a'
+	END gen 
+	FROM bronze.erp_cust_az12;
+	
+	TRUNCATE TABLE silver.erp_loc_a101;
+	-- inserting data into silver.erp_loc_a101
+	INSERT INTO silver.erp_loc_a101(cid,cntry)
+	SELECT 
+	REPLACE (cid, '-', '') cid, 
+	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+		 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+		 ELSE cntry
+	END cntry FROM bronze.erp_loc_a101;
+	
+	TRUNCATE TABLE silver.erp_px_cat_g1v2;
+	-- inserting data into silver.erp_px_cat_g1v2
+	INSERT INTO silver.erp_px_cat_g1v2(
+		id,
+		cat,
+		subcat,
+		maintenance
+	)
+	SELECT id,
+		cat,
+		subcat,
+		maintenance FROM bronze.erp_px_cat_g1v2;
+
+		end_time := clock_timestamp();
+	load_duration := end_time - start_time;
+
+RAISE NOTICE '';   -- Blank line
+
+RAISE NOTICE 'Start Time: %', start_time;
+RAISE NOTICE 'End Time: %', end_time;
+RAISE NOTICE 'Load Duration: %', load_duration;
+	RAISE NOTICE '=====================================';
+	RAISE NOTICE 'SILVER LAYER LOADED SUCCESSFULLY';
+	RAISE NOTICE '=====================================';
+
+	EXCEPTION
+    WHEN OTHERS THEN
+        RAISE NOTICE 'Silver Load Failed';
+        RAISE NOTICE 'Error: %', SQLERRM;
+        RAISE NOTICE 'SQL State: %', SQLSTATE;
+END;
+$$;
